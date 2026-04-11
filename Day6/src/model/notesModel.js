41bdd15c87dbd7fa7b17a416be53a6a68cc5accf
Hari Thapa <harithapa4654@gmail.com>
2026-04-11 13:47:10 +0530

    day 6

---
+++ Day6/src/model/notesModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const noteSchema =  new mongoose.Schema({
+    title: String,
+    description: String
+})
+
+
+ const notesModel = mongoose.model("notes" , noteSchema)
+
+ module.exports = notesModel
