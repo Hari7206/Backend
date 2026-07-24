d251609bee66878658dc1d492c4b99e2928d9cf3
Hari Thapa <harithapa4654@gmail.com>
2026-07-24 16:33:37 +0530

    docker compose

--- Docker/server.js
+++ Docker/server.js
@@ -1,8 +1,24 @@
-import express from "express"
+import express from "express";
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Home");
+});
+
+app.get("/load", (req, res) => {
+  res.send("I AM WORKING");
+});
+
+app.get("/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Hari" },
+    { id: 2, name: "John" }
+  ]);
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
 });
 
 app.listen(3000, () => {
